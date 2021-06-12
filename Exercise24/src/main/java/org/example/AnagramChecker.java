1ddf3ff648f291cccded3092fc48074099412d32
jonathan-navarr <84359494+jonathan-navarr@users.noreply.github.com>
2021-06-12 17:46:39 -0400

    Add files via upload

---
+++ Exercise24/src/main/java/org/example/AnagramChecker.java
@@ -0,0 +1,42 @@
+package org.example;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class AnagramChecker {
+    public static void main(String[] args){
+        Scanner scnr = new Scanner(System.in);
+        String firstWord, secondWord;
+        System.out.println("Enter two strings and I'll tell you if they are anagrams");
+
+        System.out.print("Enter the first string: ");
+        firstWord = scnr.nextLine();
+
+        System.out.print("Enter the second string: ");
+        secondWord = scnr.nextLine();
+
+        if(isAnagram(firstWord, secondWord))
+        {
+            System.out.print("\"" + firstWord + "\" and \"" + secondWord + "\" are anagrams");
+        }
+        else
+        {
+            System.out.print("\"" + firstWord + "\" and \"" + secondWord + "\" are not anagrams");
+        }
+    }
+    public static boolean isAnagram(String firstWord, String secondWord){
+        if(firstWord.length() != secondWord.length()){
+            return false;
+        }
+        else
+        {
+            char[] array1 = firstWord.toLowerCase().toCharArray();
+            char[] array2 = secondWord.toLowerCase().toCharArray();
+
+            Arrays.sort(array1);
+            Arrays.sort(array2);
+
+            return (Arrays.equals(array1, array2));
+        }
+    }
+
+}
